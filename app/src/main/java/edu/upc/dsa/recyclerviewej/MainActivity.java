d6a923f487f806652107b767ec320bb61cebf153
vcarl <vcarlos139@gmail.com>
2020-04-10 20:12:38 +0200

    Acaba en teoria

--- app/src/main/java/edu/upc/dsa/recyclerviewej/MainActivity.java
+++ app/src/main/java/edu/upc/dsa/recyclerviewej/MainActivity.java
@@ -3,9 +3,23 @@ package edu.upc.dsa.recyclerviewej;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -18,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //LLAMAMOS ONCREATE PADRE
         setContentView(R.layout.activity_main);//LAYOUT QUE QUEREMOS HACER
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
         recyclerView = findViewById(R.id.my_recycler_view);// OBTENEMOS EL RECYCLERVIWE
@@ -32,13 +48,47 @@ public class MainActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        List<String> input = new ArrayList<>();//AQUI SE HACE LA LLAMADA AL RETROFIT
-        for (int i = 0; i < 100; i++) {
-            input.add("Test" + i);
-        }
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost:8080/swagger/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        // define an adapter
-        mAdapter = new MyAdapter(input);
-        recyclerView.setAdapter(mAdapter);
+        api = retrofit.create(Tracks_API.class);
+
+        api.getTracks();
+
+        Button bt = findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, NewTrackActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+    public void getTracks(){
+        Call<List<Track>> call = api.getTracks();
+
+        call.enqueue(new Callback<List<Track>>() {
+            @Override
+            public void onResponse(Call<List<Track>> call, Response<List<Track>> response) {
+                if(!response.isSuccessful()){
+                    Toast toast = Toast.makeText(getApplicationContext(),response.code(),Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                trackList = response.body();
+                // define an adapter
+                mAdapter = new MyAdapter(trackList, MainActivity.this);
+                recyclerView.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Track>> call, Throwable t) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Error inesperado", Toast.LENGTH_LONG);
+                toast.show();
+
+            }
+        });
     }
 }
+
